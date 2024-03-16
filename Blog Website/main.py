3ea3c0022fb8173204a8a3a170115cabff6fe855
YashSewarik <yashsewarik1@gmail.com>
2024-03-17 00:01:10 +0530

    Connected the blog with Database

--- Blog Website/main.py
+++ Blog Website/main.py
@@ -1,5 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:@localhost/flaskblog'
+db=SQLAlchemy(app)
+
+class Contacts(db.Model):
+    sno = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(80),nullable=False)
+    email = db.Column(db.String(20),nullable=False)
+    phone_num=db.Column(db.String(12),nullable=False)
+    date=db.Column(db.String(12),nullable=True)
+    msg=db.Column(db.String(120),nullable=False)
+
+
+
 
 @app.route("/")
 def index():
@@ -11,8 +28,19 @@ def about():
     name = "rohan das"
     return render_template('about.html', name2= name)
 
-@app.route("/contact")
+@app.route("/contact",methods = ['GET','POST'])
 def contact():
+    if (request.method=='POST'):
+       name= request.form.get('name')
+       email= request.form.get('email')
+       phone= request.form.get('phone')
+       message= request.form.get('message')
+       
+       
+       entry=Contacts(name=name,phone_num=phone,email=email,date=datetime.now(),msg=message)
+       db.session.add(entry)
+       db.session.commit()
+
     return render_template('contact.html')
 
 app.run(debug=True)
