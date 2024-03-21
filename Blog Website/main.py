a6ffab58828305d4a7e302ff45fe1db5965d8849
YashSewarik <yashsewarik1@gmail.com>
2024-03-21 22:49:14 +0530

    Added pagination

--- Blog Website/main.py
+++ Blog Website/main.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from datetime import datetime
 import json
+import math
 
 
 
@@ -43,6 +44,27 @@ class Posts(db.Model):
     content = db.Column(db.String(120), nullable=False)
     date = db.Column(db.String(12), nullable=True)
 
+
+@app.route("/")
+def home():
+    posts = Posts.query.filter_by().all()
+    last = math.ceil(len(posts)/int(params['no_of_posts']))
+    page = request.args.get('page')
+    if (not str(page).isnumeric()):
+        page = 1
+    page = int(page)
+    posts = posts[(page-1)*int(params['no_of_posts']):(page-1)*int(params['no_of_posts'])+ int(params['no_of_posts'])]
+    if page==1:
+        prev = "#"
+        next = "/?page="+ str(page+1)
+    elif page==last:
+        prev = "/?page="+ str(page-1)
+        next = "#"
+    else:
+        prev = "/?page="+ str(page-1)
+        next = "/?page="+ str(page+1)
+    
+    return render_template('index.html', params=params, posts=posts, prev=prev, next=next)
 
 
 @app.route("/")
